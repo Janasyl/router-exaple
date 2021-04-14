d212ff550806db7f157ab78d913c21306968da4d
[Janasyl] <jduishenbae@gmail.com>
2021-04-14 16:36:50 +0600

    created new 3 file Home.js About.js Contact.js and add to app.js

---
+++ src/components/About/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './About.css'
+
+
+const About = () => {
+    return (
+        <div className="About">
+             <h1>About please !!!</h1>
+
+        </div>
+    )
+};
+
+export default About;
---
+++ src/components/Contacts/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+const Contact = () => {
+    return (
+        <div className="Contact">
+             <h1>Contact please !!!</h1>
+
+        </div>
+    )
+};
+
+export default Contact;
